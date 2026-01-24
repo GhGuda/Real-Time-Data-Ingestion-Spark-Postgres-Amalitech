e2cbf9fa84dd633b07ef9f4c31277f4712210dd2
GhGuda <toyboipressure1@gmail.com>
2026-01-24 22:22:59 +0000

    Minor fixes on spark_job, db and genrator

--- db/postgres_setup.sql
+++ db/postgres_setup.sql
@@ -4,10 +4,26 @@
 -- ============================================================
 
 CREATE TABLE IF NOT EXISTS ecommerce_events (
-    id INTEGER,
-    event VARCHAR(50),
-    user_name VARCHAR(50),
+    id INTEGER PRIMARY KEY,
+
+    event VARCHAR(50) NOT NULL
+        CHECK (
+            event IN (
+                'product_view',
+                'add_to_cart',
+                'remove_from_cart',
+                'checkout',
+                'purchase'
+            )
+        ),
+
+    user_name VARCHAR(50) NOT NULL,
     product VARCHAR(50),
     price DOUBLE PRECISION,
-    event_time TIMESTAMP
+
+    event_time TIMESTAMP NOT NULL,
+
+    source_file TEXT,
+
+    ingested_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
